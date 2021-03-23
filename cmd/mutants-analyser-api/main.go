a9229cd281e4e6d4c4f1bdc32ba786916b69754d
Agustin Izaguirre <Agustinizag1@gmail.com>
2021-03-23 20:51:41 -0300

    Add /mutant demo endpoint

--- cmd/mutants-analyser-api/main.go
+++ cmd/mutants-analyser-api/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"github.com/AgustinIzaguirre/mutants-analyser-api/internal/mutants/controller"
 )
 
 func main() {
 	server := gin.Default()
+	mutantGroup := server.Group("/mutant")
+	{
+		mutantController := controller.New()
+		mutantGroup.GET("/", mutantController.AnalyseDNA)
+	}
 	if error := server.Run(":5000"); error != nil {
 		log.Fatal(error.Error())
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/AgustinIzaguirre/mutants-analyser-api
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require github.com/gin-gonic/gin v1.6.3
